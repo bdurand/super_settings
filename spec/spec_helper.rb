60f2f1adb6a0ef0a21f665dcb6cda6b3d5f866dc
Brian Durand <bbdurand@gmail.com>
2021-09-12 11:30:16 -0700

    culprite settings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -69,6 +69,9 @@ end
 
 Capybara.app = TestMiddleware.new(lambda { |env| [204, {}, [""]] })
 Capybara.javascript_driver = :cuprite
+Capybara.register_driver(:cuprite) do |app|
+  Capybara::Cuprite::Driver.new(app, window_size: [1024, 800], browser_options: {"no-sandbox": nil})
+end
 WebMock.disable_net_connect!(allow_localhost: true)
 
 redis = Redis.new(url: ENV["TEST_REDIS_URL"]) if ENV["TEST_REDIS_URL"]
